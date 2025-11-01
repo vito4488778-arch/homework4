999b024725a8459dbb229ca1477eb9b041f9b04d
vito4488778-arch <vito.4488778@gmail.com>
2025-11-01 20:07:05 +0800

    Initial Commit

---
+++ homework4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  homework4
+//
+//  Created by 114iosClassStudent04 on 2025/11/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ homework4/homework4App.swift
@@ -0,0 +1,17 @@
+//
+//  homework4App.swift
+//  homework4
+//
+//  Created by 114iosClassStudent04 on 2025/11/1.
+//
+
+import SwiftUI
+
+@main
+struct homework4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
